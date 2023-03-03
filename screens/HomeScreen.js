35ce88beaa225ceadf97d873c12c6d5c34e5b6e6
ihor-vip <vip20140902@gmail.com>
2023-03-03 13:02:06 +0200

    first section of home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,27 @@
-import {View, Text} from "react-native";
+import {View, Text, SafeAreaView} from "react-native";
+import {useNavigation} from "@react-navigation/native";
+import {useLayoutEffect} from "react";
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation()
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    },[])
+
     return (
-        <View>
-            <Text className="text-red-400">
-                HomeScreen
-            </Text>
-        </View>
+       <SafeAreaView className="bg-white flex-1 relative">
+           <View className="flex-row px-6 mt-8 items-center space-x-2">
+               <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+                   <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
+               </View>
+
+               <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+           </View>
+       </SafeAreaView>
     )
 }
 
