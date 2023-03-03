4de708b1db611ca931c936308788e271bee1cdeb
ihor-vip <vip20140902@gmail.com>
2023-03-03 14:15:33 +0200

    first section of discover screen

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,10 +1,32 @@
-import {Text, View} from "react-native";
+import {SafeAreaView, Text, View, Image} from "react-native";
+import React, {useLayoutEffect} from "react";
+import {useNavigation} from "@react-navigation/native";
+import {Avatar} from "../assets";
 
 const Discover = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
     return (
-        <View>
-            <Text>Discover</Text>
-        </View>
+        <SafeAreaView className="flex-1 bg-white relative">
+            <View className="flex-row items-center justify-between px-8">
+                <View>
+                    <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+                    <Text className="text-[#527283] text-[36px]">the beauty today</Text>
+                </View>
+
+                <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-lg">
+                    <Image
+                        source={Avatar}
+                        className="w-full h-full rounded-md object-cover"
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
