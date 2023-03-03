36afc3e95865682a6f3bfe684026e292ce0fad3b
ihor-vip <vip20140902@gmail.com>
2023-03-03 13:30:17 +0200

    second section of home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,12 +14,27 @@ const HomeScreen = () => {
 
     return (
        <SafeAreaView className="bg-white flex-1 relative">
+           {/* First Section */}
+
            <View className="flex-row px-6 mt-8 items-center space-x-2">
                <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
                    <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
                </View>
 
                <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+           </View>
+
+           {/* Second Section */}
+           <View className="px-6 mt-8 space-y-3">
+               <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+               <Text className="text-[#00BCC9] text-[38px] font-bold">
+                   Good Moments
+               </Text>
+
+               <Text className="text-[#3C6072] text-base">
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio
+                   quis nostrum
+               </Text>
            </View>
        </SafeAreaView>
     )
