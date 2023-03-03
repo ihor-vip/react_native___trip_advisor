bfda53d4a5d8f73af5896ad394a6a87b51e8df8d
ihor-vip <vip20140902@gmail.com>
2023-03-03 13:41:23 +0200

    adding to home screen image container

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,43 +1,61 @@
-import {View, Text, SafeAreaView} from "react-native";
-import {useNavigation} from "@react-navigation/native";
-import {useLayoutEffect} from "react";
+import {
+    View,
+    Text,
+    SafeAreaView,
+    Image,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
+
+import { useNavigation } from "@react-navigation/native";
+import { HeroImage } from "../assets";
 
 const HomeScreen = () => {
-
-    const navigation = useNavigation()
+    const navigation = useNavigation();
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerShown: false,
-        })
-    },[])
+        });
+    }, []);
 
     return (
-       <SafeAreaView className="bg-white flex-1 relative">
-           {/* First Section */}
+        <SafeAreaView className="bg-white flex-1 relative">
+            {/* First Section */}
 
-           <View className="flex-row px-6 mt-8 items-center space-x-2">
-               <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
-                   <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
-               </View>
+            <View className="flex-row px-6 mt-8 items-center space-x-2">
+                <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+                    <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
+                </View>
 
-               <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
-           </View>
+                <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+            </View>
 
-           {/* Second Section */}
-           <View className="px-6 mt-8 space-y-3">
-               <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
-               <Text className="text-[#00BCC9] text-[38px] font-bold">
-                   Good Moments
-               </Text>
+            {/* Second Section */}
+            <View className="px-6 mt-8 space-y-3">
+                <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+                <Text className="text-[#00BCC9] text-[38px] font-bold">
+                    Good Moments
+                </Text>
 
-               <Text className="text-[#3C6072] text-base">
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio
-                   quis nostrum
-               </Text>
-           </View>
-       </SafeAreaView>
-    )
-}
+                <Text className="text-[#3C6072] text-base">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio
+                    quis nostrum
+                </Text>
+            </View>
+
+            {/* Circle Section */}
+            <View className="w-[400px] h-[400px] bg-[#00BCC9] rounded-full absolute bottom-36 -right-36"></View>
+            <View className="w-[400px] h-[400px] bg-[#E99265] rounded-full absolute -bottom-28 -left-36"></View>
+
+            {/* Image container */}
+            <View className="flex-1 relative items-center justify-center">
+                <Image
+                    source={HeroImage}
+                    className="w-full h-full object-cover mt-20"
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
 
 export default HomeScreen;
